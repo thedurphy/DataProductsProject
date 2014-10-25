c580b89080d1f69edc1a1bda2256d678759c1679
thedurphy <thedurphy@hotmail.com>
2014-10-25 04:51:24 -0500

    second commit

--- ui.R
+++ ui.R
@@ -1,6 +1,7 @@
 shinyUI(pageWithSidebar(
         headerPanel("How many miles-per-gallon are YOU getting?...in the 1970s"),
                 sidebarPanel(
+                        h4('Adjust the attributes below to see your fuel efficiency'),
                 sliderInput('cylinders', 'How many cylinders?', value = 5.5, min = 2, max = 12, step = 1,),
                 sliderInput('displacement', 'How large is the engine? (cubic Inches)', value = 194, min = 68, max = 460, step = 1,),
                 sliderInput('horsepower', 'What horsepower is your car?', value = 105, min = 46, max = 230, step = 1,),
@@ -10,6 +11,7 @@ shinyUI(pageWithSidebar(
         mainPanel(
                 h5('As gas prices are soaring, it is even more important to get the most out of the gas you buy.  This handy app will take the following attributes of your automobile and output you miles-per-gallon.  You can also play around with sliders to see what attribute can contribute the most in getting the most out of your vehicle.'),
                 h4('Miles Per Gallon'),
-                verbatimTextOutput("prediction")
+                verbatimTextOutput("prediction"),
+                h6('Please give the app at least a minute the first time.  It is running a prediction algorithm which will take a bit.')
                 )
         ))
